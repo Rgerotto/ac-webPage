84b6cb09a7275615a9ddfdb1f21a9c5c306040d1
Rafael Gerotto <rafael.gerotto@hotmail.com>
2024-10-15 21:35:04 +0200

    css done

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-// script.js
+const button = document.querySelector('#submitBtn');
+const inputName = document.querySelector('#name');
+const inputEmail = document.querySelector('#email');
+const inputPhone = document.querySelector('#phone');
+const inputSubject = document.querySelector('#subject');
+const inputMessage = document.querySelector('#message');
+
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+  const name = inputName.value;
+  const email = inputEmail.value;
+  const phone = inputPhone.value;
+  const subject = inputSubject.value;
+  const message = inputMessage.value;
+
+  const formattedMessage = `Nome: ${name}%0AEmail: ${email}%0ATelefone: ${phone}%0AAssunto: ${subject}%0AMensagem: ${message}`;
+
+  const whatsappNumber = '+34685532955';
+  window.open(`https://wa.me/${whatsappNumber}?text=${formattedMessage}`, '_blank');
+})
+
 document.addEventListener('DOMContentLoaded', function () {
     const sections = document.querySelectorAll('.section');
   
@@ -6,19 +27,18 @@ document.addEventListener('DOMContentLoaded', function () {
       (entries) => {
         entries.forEach(entry => {
           if (entry.isIntersecting) {
-            // Se está visível, aplica a animação
+            
             if ([...sections].indexOf(entry.target) % 2 === 0) {
-              entry.target.classList.add('reveal-right'); // Seções pares vêm da direita
+              entry.target.classList.add('reveal-right');
             } else {
-              entry.target.classList.add('reveal-left'); // Seções ímpares vêm da esquerda
+              entry.target.classList.add('reveal-left');
             }
           } else {
-            // Se não está mais visível, remove a classe de animação para desaparecer
             entry.target.classList.remove('reveal-left', 'reveal-right');
           }
         });
       },
-      { threshold: 0.1 } // 10% de visibilidade para ativar
+      { threshold: 0.1 }
     );
   
     sections.forEach(section => {
